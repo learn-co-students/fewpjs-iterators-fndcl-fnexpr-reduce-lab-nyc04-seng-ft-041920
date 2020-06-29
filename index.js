c0723a36f435427ddb2a3fbb9f78d63a585b371c
JonTDean <JonathanTomDean@Gmail.com>
2020-06-29 07:25:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// Why the 50min read on Reduce???
+let totalBatteries = batteryBatches.reduce((accum, val) => accum + val)
